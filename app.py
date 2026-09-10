bb3d6cefa549eaa246e842d5896bbcadd18f7e0d
xMFMSx <morales.sepulveda.manuel@gmail.com>
2026-09-10 02:14:53 -0300

    Update app.py

--- app.py
+++ app.py
@@ -98,7 +98,7 @@ div[data-testid="stHorizontalBlock"]:has(.fila-datos) {
     padding-top: 6px !important;
     padding-bottom: 0px !important;
     width: 100% !important;
-    min-height: 16px !important;
+    min-height: 10px !important;
 }
 
 /* Forzar anchos mínimos exactos anti-móvil */
