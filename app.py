f14a67d406360c2c60af625ac16e74c10a21beaf
xMFMSx <morales.sepulveda.manuel@gmail.com>
2026-09-10 04:12:33 -0300

    Update app.py

--- app.py
+++ app.py
@@ -107,7 +107,7 @@ div[data-testid="stHorizontalBlock"]:has(.contenedor-tabla) > div[data-testid="c
 /* Celdas individuales */
 .contenedor-tabla > div {
     border-right: 1px solid #353b4d !important;
-    padding: 0 0px !important;
+    padding: 0 2px !important;
     height: 100% !important;
     display: flex !important;
     align-items: center !important;
