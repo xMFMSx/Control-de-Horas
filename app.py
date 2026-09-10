2607441f4924edde6f95ab6f93343440befd1d95
xMFMSx <morales.sepulveda.manuel@gmail.com>
2026-09-10 04:19:29 -0300

    Update app.py

--- app.py
+++ app.py
@@ -80,7 +80,7 @@ div[data-testid="stHorizontalBlock"]:has(.contenedor-tabla) > div[data-testid="c
 /* 4. Estilos de la Cuadrícula HTML (Interna con CSS GRID INQUEBRANTABLE) */
 .contenedor-tabla {
     display: grid !important;
-    grid-template-columns: 3.5% 8% 8% 8% 16% 30% !important; /* <--- Proporciones bloqueadas con candado */
+    grid-template-columns: 3.5% 8% 6% 10% 10% 15% !important; /* <--- Proporciones bloqueadas con candado */
     width: 100% !important;
     height: 38px !important;
     align-items: center !important;
