c7e119cfc81ee01d8be297b568910594943cf86a
xMFMSx <morales.sepulveda.manuel@gmail.com>
2026-09-10 02:42:11 -0300

    Update app.py

--- app.py
+++ app.py
@@ -85,7 +85,7 @@ div.stElementContainer:has(.encabezado-puro) {
 /* --- 2. FILAS DE DATOS Y COLUMNAS --- */
 div.element-container:has(div[data-testid="stHorizontalBlock"]:has(.fila-datos)),
 div.stElementContainer:has(div[data-testid="stHorizontalBlock"]:has(.fila-datos)) {
-    margin-top: -24px !important; /* <--- Ahora sí succiona TODA la fila completa hacia arriba */
+    margin-top: -12px !important; /* <--- Ahora sí succiona TODA la fila completa hacia arriba */
     margin-bottom: 0px !important;
 }
 div[data-testid="stHorizontalBlock"]:has(.fila-datos) {
