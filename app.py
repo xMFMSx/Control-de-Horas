aa478410e4dfbe24e658d21df17bcefaf1d17127
xMFMSx <morales.sepulveda.manuel@gmail.com>
2026-09-11 20:21:01 -0300

    Update app.py

--- app.py
+++ app.py
@@ -172,7 +172,7 @@ div[data-testid="stVerticalBlock"]:has(div.es-datos-6) {
 }
 
 div[data-testid="stVerticalBlock"]:has(div.es-datos-6) > div {
-    margin-bottom: -7px !important;
+    margin-bottom: -6px !important;
     padding-bottom: 0 !important;
     padding-top: 0 !important;
 }
