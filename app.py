0192231f97d536cfc99a14002ed9615c83359504
xMFMSx <morales.sepulveda.manuel@gmail.com>
2026-09-11 20:30:39 -0300

    Update app.py

--- app.py
+++ app.py
@@ -793,7 +793,7 @@ else:
                         </style>
                         ''', unsafe_allow_html=True)
                         
-                        st.markdown('<div class="lapiz-lateral-wrapper" style="padding-top: 6px;">', unsafe_allow_html=True)
+                        st.markdown('<div class="lapiz-lateral-wrapper" style="padding-top: 10px;">', unsafe_allow_html=True)
                         if st.button("✏️", key=f"edit_btn_{d}"):
                             if st.session_state.get("dia_en_edicion") == d:
                                 st.session_state["dia_en_edicion"] = None
