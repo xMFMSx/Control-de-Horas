1aa9956078169debc94f5fb6eae5b02eff57a080
xMFMSx <morales.sepulveda.manuel@gmail.com>
2026-09-10 23:27:04 -0300

    Update app.py

--- app.py
+++ app.py
@@ -110,8 +110,8 @@ div[data-testid="stMarkdownContainer"] p {
 
 /* Forzar que la columna derecha del botón sea estrecha y compacta */
 div[data-testid="stHorizontalBlock"]:has(.es-datos) > div[data-testid="column"]:last-child {
-    flex: 0 0 40px !important;
-    max-width: 40px !important;
+    flex: 0 0 10px !important;
+    max-width: 10px !important;
     margin: 1 auto !important;
 }
 
