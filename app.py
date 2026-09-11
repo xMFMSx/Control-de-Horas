3a4032765d9b1399f4a71dc10ea91b1cdfa65270
xMFMSx <morales.sepulveda.manuel@gmail.com>
2026-09-11 18:08:43 -0300

    Update app.py

--- app.py
+++ app.py
@@ -98,7 +98,7 @@ div[data-testid="stHorizontalBlock"]:has(.contenedor-tabla) > div[data-testid="c
 /* Estructura CSS Grid para la tabla de datos */
 .contenedor-tabla {
     display: grid !important;
-    grid-template-columns: 6% 10% 8% 9% 8% 15% !important;
+    grid-template-columns: 6% 10% 8% 9% 8% 20% !important;
     width: 100% !important;
     align-items: center !important;
     box-sizing: border-box !important;
