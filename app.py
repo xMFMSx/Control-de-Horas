597aff2e5e2284bc3f3444ce5e42bb91b33d03c6
xMFMSx <morales.sepulveda.manuel@gmail.com>
2026-09-11 18:36:57 -0300

    Update app.py

--- app.py
+++ app.py
@@ -696,7 +696,7 @@ else:
 
             if registros_tabla:
                 # --- ENCABEZADO PERFECTAMENTE UNIFICADO ---
-                c_h1, c_h2 = st.columns([0.82, 0.18], vertical_alignment="center")
+                c_h1, c_h2 = st.columns([0.80, 0.20], vertical_alignment="center")
                 with c_h1:
                     st.markdown('''
                     <div class="contenedor-tabla es-encabezado">
@@ -738,7 +738,7 @@ else:
                     hn_val = r["HORA EXTRA"].strip() if r["HORA EXTRA"].strip() else "&nbsp;"
                     hr_val = r["HORA RECARGO"].strip() if r["HORA RECARGO"].strip() else "&nbsp;"
 
-                    c_dat, c_b = st.columns([0.82, 0.18], vertical_alignment="center")
+                    c_dat, c_b = st.columns([0.80, 0.20], vertical_alignment="center")
                     with c_dat:
                         st.markdown(f'''
                         <div class="es-datos">
