7abc4742cda98c0c0940e1d56794e1f8e3f41562
xMFMSx <morales.sepulveda.manuel@gmail.com>
2026-09-11 19:39:10 -0300

    Update app.py

--- app.py
+++ app.py
@@ -59,7 +59,7 @@ div[data-testid="stVerticalBlock"] {
    ========================================================== */
 .contenedor-tabla-7 {
     display: grid !important;
-    grid-template-columns: 8% 16% 16% 16% 16% 18% 10% !important;
+    grid-template-columns: 6% 12% 12% 12% 12% 46% 10% !important;
     width: 100% !important;
     align-items: center !important;
     box-sizing: border-box !important;
